e63240fecf64798dfbce51b125f2248d3d9b3904
PavanBojja <pavanbojja@gmail.com>
2026-02-12 09:15:46 +0530

    weather history

---
+++ 12feb.js
@@ -0,0 +1,5 @@
+// a.Create custom / local modules and export them using various module patterns.
+// b.Explore the functionality of os, path, util and events modules.
+// c.Use the fs module for creating directories and files of different formats.
+// d.Write script to read and write the streaming data using readable and writable streams
+
